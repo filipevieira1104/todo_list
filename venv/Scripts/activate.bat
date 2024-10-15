784ab7a74008f68286289ec170c1d2d4c352df90
Filipe <you@example.com>
2024-10-15 14:24:21 -0300

    Atualizações no arquico html

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\filip\Desktop\projetos\gerenciador_tarefas\venv
+set VIRTUAL_ENV=C:\Users\V8\todo_list\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
